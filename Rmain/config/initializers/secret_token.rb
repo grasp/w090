c96671317c0c9bf8ebb53ef4504ea2857508cf38
hunter <hunter.wxhu@gmail.com>
2012-04-09 17:37:10 +0800

    add Rmain

---
+++ Rmain/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rmain::Application.config.secret_token = '397182658d3da81de691f4277427d429802f4b4acd25a9288a969e02d42db843cf55d8ef6027e2c15b8a8a5d185f3dea6341d079d7ad11fb41d42b6ed4bbe98a'
